49c726b07d90f5c199b127c3b0c8051d322a5dbc
claudiabdm <claudiabdm@hotmail.es>
2024-04-04 15:22:27 +0200

    Disable dev toolbar

--- demo/astro.config.ts
+++ demo/astro.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+    devToolbar: {
+        enabled: false
+    },
     image: {
         domains: ["a.storyblok.com"],
         service: {
